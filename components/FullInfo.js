0aee4ced22322063ac64199bc7e245b9b3a0f213
George <jorjio21@gmail.com>
2022-04-18 12:25:20 +0300

    add work with data

--- components/FullInfo.js
+++ components/FullInfo.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
-import { globalStyle } from "./../styles/style";
+import { globalStyle } from "../styles/style";
 
-export default function Contacts({ navigation }) {
+export default function FullInfo({ route, navigation }) {
   return (
     <View style={globalStyle.main}>
-      <Text style={globalStyle.tittle}>Contacts Page</Text>
-      <Button
+      <Text style={globalStyle.tittle}>{route.params.name}</Text>
+      <Text>{route.params.full}</Text>
+
+      {/* <Button
         title="Открыть страницу Main"
         onPress={() => navigation.goBack()} //простое возвращение назад
-      />
+      /> */}
     </View>
   );
 }
--- components/Main.js
+++ components/Main.js
@@ -1,15 +1,34 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableHighlight, FlatList } from "react-native";
 import { globalStyle } from "./../styles/style";
 
 export default function Main({ navigation }) {
+  const [news, setNews] = useState([
+    { name: "Google", anonce: "Google!!!!", full: "Google is cool!" },
+    { name: "Apple", anonce: "Apple!!!!", full: "Apple is cool!" },
+    { name: "Facebook", anonce: "Facebook!!!!", full: "Facebook is cool!" },
+  ]);
+
   return (
     <View style={globalStyle.main}>
       <Text style={globalStyle.tittle}>Main Page</Text>
-      <Button
+      <FlatList
+        data={news}
+        renderItem={({ item }) => {
+          return (
+            <TouchableHighlight
+              onPress={() => navigation.navigate("FullInfo", item)}
+            >
+              <Text>{item.name}</Text>
+            </TouchableHighlight>
+          );
+        }}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      {/* <Button
         title="Открыть страницу Contacts"
         onPress={() => navigation.navigate("Contacts")} //переход на конкретную страницу
-      />
+      /> */}
     </View>
   );
 }
