6ce0d300ba655a15c5405573baaff575c3bb1c67
George <jorjio21@gmail.com>
2022-04-18 10:23:25 +0300

    add main page and preloader

---
+++ styles/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyle = StyleSheet.create({
+  main: {
+    flex: 1,
+    padding: 20,
+    paddingTop: 50,
+  },
+  tittle: {
+    fontSize: 20,
+    color: "#333",
+    fontFamily: "mtBold",
+    textAlign: "center",
+  },
+});
